219e8c6c9cdef715cd5154926e516a7c8d5f4543
Dennis Muturia <dmuturia2922@gmail.com>
2021-09-21 21:44:12 +0200

    cleanup done

--- app/src/main/java/com/dennis/authentication_app/CreateUser.java
+++
@@ -1,9 +0,0 @@
-package com.dennis.authentication_app;
-
-import com.google.firebase.auth.FirebaseUser;
-
-public interface CreateUser {
-    FirebaseUser createUser();
-    void emailVerification();
-    FirebaseUser signIn();
-}
